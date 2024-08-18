694d37ad5c3eca82e3dd4d02d741ba4bb25e9dd8
Samarth-4403 <samarth04042003@gmail.com>
2024-08-18 13:08:54 +0530

    Fourth commit

--- app.py
+++ app.py
@@ -8,26 +8,43 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    todo_list = Todo.query.all()
-    return render_template("base.html", todo_list=todo_list)
+    search_query = request.args.get('search')
+    filter_status = request.args.get('filter')
+    page = request.args.get('page', 1, type=int)
+    per_page = 5  # Number of tasks per page
+
+    query = Todo.query
+
+    if search_query:
+        query = query.filter(Todo.title.contains(search_query))
+
+    if filter_status == "complete":
+        query = query.filter_by(complete=True)
+    elif filter_status == "incomplete":
+        query = query.filter_by(complete=False)
+
+    pagination = query.paginate(page=page, per_page=per_page)
+    todo_list = pagination.items
+
+    return render_template("base.html", todo_list=todo_list, pagination=pagination, search_query=search_query, filter_status=filter_status)
 
 
 @app.route("/add", methods=["POST"])
 def add():
     title = request.form.get("title")
+    page = request.args.get('page', 1)
     new_todo = Todo(title=title, complete=False)
     db.session.add(new_todo)
     db.session.commit()
-    return redirect(url_for("home"))
+    return redirect(url_for("home", page=page))
 
 
 @app.route("/update/<int:todo_id>")
@@ -35,7 +52,8 @@ def update(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
     todo.complete = not todo.complete
     db.session.commit()
-    return redirect(url_for("home"))
+    page = request.args.get('page', 1)
+    return redirect(url_for("home", page=page))
 
 
 @app.route("/delete/<int:todo_id>")
@@ -43,7 +61,8 @@ def delete(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
     db.session.delete(todo)
     db.session.commit()
-    return redirect(url_for("home"))
+    page = request.args.get('page', 1)
+    return redirect(url_for("home", page=page))
 
 if __name__ == "__main__":
     db.create_all()
